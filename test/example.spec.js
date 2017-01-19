215640f379d93682bcaf98af134f95ab66603d88
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-01-19 15:37:36 -0200

    Add utils folder and assertion library

--- test/example.spec.js
+++ test/example.spec.js
@@ -1,7 +1,9 @@
 const { describe, it } = require('mocha')
+const { expect } = require('./utils/assertion')
 
 describe('Test Example', () => {
   it('should pass all the tests', () => {
-    return true
+    expect(2 + 2).to.equal(4)
   })
 })
+
