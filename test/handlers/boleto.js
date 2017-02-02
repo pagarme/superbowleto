f4f43a12fb095939f3a873b99165d3416d541f08
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-02-02 16:27:23 -0200

    Add unit tests for handlers, lib and services

---
+++ test/handlers/boleto.js
@@ -0,0 +1,22 @@
+import test from 'ava'
+import { promisify } from 'bluebird'
+import * as boleto from '../../src/handlers/boleto'
+
+const create = promisify(boleto.create)
+const show = promisify(boleto.show)
+
+test('creates a boleto', async (t) => {
+  const { statusCode, body } = await create({}, {})
+
+  t.is(statusCode, 201)
+  t.is(typeof body, 'string')
+  t.notThrows(() => JSON.parse(body), 'should be a valid JSON string')
+})
+
+test('shows a boleto', async (t) => {
+  const { statusCode, body } = await show({}, {})
+
+  t.is(statusCode, 200)
+  t.is(typeof body, 'string')
+  t.notThrows(() => JSON.parse(body), 'should be a valid JSON string')
+})
