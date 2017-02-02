fd9f05b51f4976a52f3110ab49c3011d677ff06e
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-02-02 16:28:44 -0200

    Change test infrastructure to avajs and remove mocha

--- test/example.spec.js
+++
@@ -1,9 +0,0 @@
-const { describe, it } = require('mocha')
-const { expect } = require('./utils/assertion')
-
-describe('Test Example', () => {
-  it('should pass all the tests', () => {
-    expect(2 + 2).to.equal(4)
-  })
-})
-
