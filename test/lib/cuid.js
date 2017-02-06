dff6c94daacdedc42e835eb768625936502f5afe
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-02-06 20:31:47 -0200

    Add cuid helper

---
+++ test/lib/cuid.js
@@ -0,0 +1,23 @@
+import test from 'ava'
+import { defaultCuidValue } from '../../src/lib/cuid'
+
+test('creates a cuid without prefix', async (t) => {
+  const createCuid = defaultCuidValue()
+  const cuid = createCuid()
+
+  t.is(typeof createCuid, 'function')
+  t.is(typeof cuid, 'string')
+  t.is(cuid.length, 25)
+})
+
+test('creates a cuid with prefix', async (t) => {
+  const prefix = 'test_'
+  const prefixLength = prefix.length
+  const createCuid = defaultCuidValue(prefix)
+  const cuid = createCuid()
+
+  t.is(typeof createCuid, 'function')
+  t.is(typeof cuid, 'string')
+  t.is(cuid.length, 25 + prefix.length, 'should have a length of 25 without the prefix')
+  t.is(cuid.substring(0, prefixLength), 'test_', 'should start with prefix')
+})
