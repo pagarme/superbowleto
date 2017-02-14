c8efe7b73df48bbe4a3b74665309178806aa16d4
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-02-14 12:19:44 -0200

    Add pagination helper
    closes #40

---
+++ test/unit/database/helpers/pagination.js
@@ -0,0 +1,23 @@
+import test from 'ava'
+import { getPaginationQuery } from '../../../../src/database/helpers/pagination'
+
+test('creates a pagination with defaults', async (t) => {
+  const pagination = getPaginationQuery()
+
+  t.deepEqual(pagination, {
+    limit: 10,
+    offset: 0
+  })
+})
+
+test('creates a pagination with `page = 3` and `count = 15`', async (t) => {
+  const pagination = getPaginationQuery({
+    page: 3,
+    count: 15
+  })
+
+  t.deepEqual(pagination, {
+    limit: 15,
+    offset: 30
+  })
+})
