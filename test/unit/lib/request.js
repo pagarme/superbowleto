4bcb1e423a5d4b884e2a277f7df35002a6fbaa6f
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-02-22 17:30:10 -0300

    Add request parameter validation and coercion

---
+++ test/unit/lib/request.js
@@ -0,0 +1,62 @@
+import test from 'ava'
+import Joi from 'joi'
+import { assert } from '../../helpers/chai'
+import { ValidationError } from '../../../src/resources/errors'
+import { parse } from '../../../src/lib/request'
+
+const schema = {
+  name: Joi
+    .string()
+    .required(),
+
+  security_number: Joi
+    .number()
+}
+
+test('parse: with valid schema', async (t) => {
+  const value = await parse(schema, {
+    name: 'John Appleseed',
+    security_number: 42
+  })
+
+  t.deepEqual(value, {
+    name: 'John Appleseed',
+    security_number: 42
+  }, 'should have the correct parsed parameters and no errors')
+})
+
+test('parse: with valid schema and coercion', async (t) => {
+  const value = await parse(schema, {
+    name: 'John Appleseed',
+    security_number: '42'
+  })
+
+  t.deepEqual(value, {
+    name: 'John Appleseed',
+    security_number: 42
+  }, 'should have the correct parsed, coerced parameters and no errors')
+})
+
+test('parse: with invalid schema', async (t) => {
+  const validationError = await parse(schema, {
+    security_number: true
+  }).catch(err => err)
+
+  const { errors } = validationError
+
+  t.true(validationError instanceof ValidationError, 'should be a ValidationError')
+  t.true(Array.isArray(errors), 'should have an array of errors')
+  t.is(errors.length, 2, 'should have 2 errors')
+
+  assert.containSubset(errors[0], {
+    message: '"name" is required',
+    type: 'invalid_parameter',
+    field: 'name'
+  }, 'should have an error because `name` is invalid')
+
+  assert.containSubset(errors[1], {
+    message: '"security_number" must be a number',
+    type: 'invalid_parameter',
+    field: 'security_number'
+  }, 'should have an error because `security_number` is invalid')
+})
