c8be65aed3dfcba3eac4e3c62453e5742fc7ae80
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-02-23 13:57:29 -0300

    Add queue request validation and schema

--- test/e2e/queue/create.js
+++ test/e2e/queue/create.js
@@ -6,7 +6,7 @@ import * as queueHandler from '../../../src/resources/queue/handler'
 
 const createQueue = normalizeHandler(queueHandler.create)
 
-test('creates a queue', async (t) => {
+test('creates a queue with valid data', async (t) => {
   const { body, statusCode } = await createQueue({
     body: queueMock
   })
@@ -14,4 +14,25 @@ test('creates a queue', async (t) => {
   t.is(statusCode, 201)
   t.is(body.object, 'queue')
   assert.containSubset(body, queueMock)
+})
+
+test('creates a queue with invalid data', async (t) => {
+  const { body, statusCode } = await createQueue({
+    body: {
+      name: 123
+    }
+  })
+
+  t.is(statusCode, 400)
+  assert.containSubset(body, {
+    errors: [{
+      type: 'invalid_parameter',
+      field: 'url',
+      message: '"url" is required'
+    }, {
+      type: 'invalid_parameter',
+      field: 'name',
+      message: '"name" must be a string'
+    }]
+  })
 })
