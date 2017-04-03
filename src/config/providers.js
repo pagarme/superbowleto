484176d776e57849c6f506525d3fdc46e615c251
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-04-03 14:16:04 -0300

    Add providers config
    Add providers config structure with bradesco config for test and
    development

---
+++ src/config/providers.js
@@ -0,0 +1,28 @@
+import { getConfig } from './index'
+
+const config = getConfig({
+  development: {
+    bradesco: {
+      endpoint: 'https://homolog.meiosdepagamentobradesco.com.br/registro_boleto/api',
+      merchantId: '100005254',
+      securityKey: 'bbE9XN8RhOyA9-79HHPnbJ1-Qqy7kzoKGdR-Njmi9fg'
+    }
+  },
+  production: {
+    bradesco: {
+      endpoint: 'https://meiosdepagamentobradesco.com.br/registro/api',
+      merchantId: 'MERCHANT_ID',
+      securityKey: 'SECURITY_KEY'
+    }
+  },
+  test: {
+    bradesco: {
+      endpoint: 'https://homolog.meiosdepagamentobradesco.com.br/registro_boleto/api',
+      merchantId: '100005254',
+      securityKey: 'bbE9XN8RhOyA9-79HHPnbJ1-Qqy7kzoKGdR-Njmi9fg'
+    }
+  }
+})
+
+export default config
+
