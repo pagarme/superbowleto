7e0cc0df8be02fc1cf14e6b570a86bbeb24d902d
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-04-05 15:06:23 -0300

    bradesco: add buildPayload function to provider
    Add a function buildPayload to create the payload needed for bradesco
    provider's register function.

--- src/providers/bradesco/index.js
+++ src/providers/bradesco/index.js
@@ -1,4 +1,5 @@
-import { prop } from 'ramda'
+import { always, compose, prop } from 'ramda'
+import { format, formatWithTemplate } from './formatter'
 import getConfig from '../../config/providers'
 import { encodeBase64 } from '../../lib/encoding'
 
@@ -11,3 +12,29 @@ export const buildHeaders = () => {
     Authorization: `Basic ${authorization}`
   }
 }
+
+export const buildPayload = formatWithTemplate({
+  merchant_id: always(merchantId),
+  boleto: {
+    carteira: always('25'),
+    nosso_numero: prop('title_id'),
+    numero_documento: prop('title_id'),
+    data_emissao: compose(format('date'), prop('created_at')),
+    data_vencimento: compose(format('date'), prop('expiration_date')),
+    valor_titulo: prop('amount'),
+    pagador: {
+      nome: prop('payer_name'),
+      documento: prop('payer_document_number'),
+      tipo_documento: compose(format('documentType'), prop('payer_document_type')),
+      endereco: {
+        cep: always('05420000'),
+        logradouro: always('Avenida Pedroso de Morais'),
+        numero: always('142'),
+        complemento: always('Ap 201'),
+        bairro: always('Pinheiros'),
+        cidade: always('São Paulo'),
+        uf: always('SP')
+      }
+    }
+  }
+})
--- test/integration/providers/bradesco/index.js
+++ test/integration/providers/bradesco/index.js
@@ -1,5 +1,13 @@
 import test from 'ava'
-import { buildHeaders } from '../../../../src/providers/bradesco'
+import moment from 'moment'
+import { createBoleto } from '../../../functional/boleto/helpers'
+import { buildHeaders, buildPayload } from '../../../../src/providers/bradesco'
+
+let boleto
+
+test.before(async () => {
+  boleto = await createBoleto()
+})
 
 test('buildHeaders', (t) => {
   const headers = buildHeaders()
@@ -9,3 +17,32 @@ test('buildHeaders', (t) => {
   })
 })
 
+test('buildPayload', async (t) => {
+  const payload = buildPayload(boleto)
+
+  t.deepEqual(payload, {
+    merchant_id: '100005254',
+    boleto: {
+      carteira: '25',
+      nosso_numero: boleto.title_id,
+      numero_documento: boleto.title_id,
+      data_emissao: moment(boleto.created_at).format('YYYY-MM-DD'),
+      data_vencimento: moment(boleto.expiration_date).format('YYYY-MM-DD'),
+      valor_titulo: 2000,
+      pagador: {
+        nome: 'David Bowie',
+        documento: '98154524872',
+        tipo_documento: '1',
+        endereco: {
+          cep: '05420000',
+          logradouro: 'Avenida Pedroso de Morais',
+          numero: '142',
+          complemento: 'Ap 201',
+          bairro: 'Pinheiros',
+          cidade: 'São Paulo',
+          uf: 'SP'
+        }
+      }
+    }
+  })
+})
