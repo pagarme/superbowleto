80d0e02a1d1024af3812adb29c69e7100b179edf
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-06-09 16:27:41 -0300

    infra: add api-gateway endpoint module
    Add a module that it's going to be used to build every endpoint of the
    API with method, lambda_integration and authorization.

---
+++ infrastructure/api-gateway/endpoint/variables.tf
@@ -0,0 +1,23 @@
+variable "rest_api_id" {
+  type = "string"
+}
+
+variable "resource_id" {
+  type = "string"
+}
+
+variable "resource_path" {
+  type = "string"
+}
+
+variable "method" {
+  type = "string"
+}
+
+variable "stage" {
+  type = "string"
+}
+
+variable "lambda_name" {
+  type = "string"
+}
