c26f531fcba37fcaad993da4d2d47aa94390a7d9
Marquinho Almeida <marco.almeida.junior@gmail.com>
2017-07-20 18:02:23 -0300

    provider: test when register on provider timeout

--- test/functional/boleto/register/pending.js
+++ test/functional/boleto/register/pending.js
@@ -1,23 +1,22 @@
 import test from 'ava'
 import { Promise, promisify } from 'bluebird'
-import { mockFunction, restoreFunction } from '../../../helpers/boleto'
+import { always } from 'ramda'
+import { mock, mockFunction, restoreFunction } from '../../../helpers/boleto'
 import { normalizeHandler } from '../../../helpers/normalizer'
 import * as boletoHandler from '../../../../build/resources/boleto'
 import * as provider from '../../../../build/providers/bradesco'
-
-test.before(async () => {
-  mockFunction(provider, 'register', () => Promise.resolve({ status: 'unknown' }))
-})
-
-test.after(() => {
-  restoreFunction(provider, 'register')
-})
 
 const create = normalizeHandler(boletoHandler.create)
 
 const register = promisify(boletoHandler.register)
 
 test('registers a boleto (provider pending)', async (t) => {
+  mockFunction(
+    provider,
+    'register',
+    always(Promise.resolve({ status: 'unknown' }))
+  )
+
   const payload = {
     expiration_date: new Date(),
     amount: 2000,
@@ -34,8 +33,42 @@ test('registers a boleto (provider pending)', async (t) => {
   })
 
   const boleto = await register({
-    body: JSON.stringify({ boleto_id: body.id, sqsMessage: { ReceiptHandle: 'abc' } })
+    body: JSON.stringify({
+      boleto_id: body.id,
+      sqsMessage: {
+        ReceiptHandle: 'abc'
+      }
+    })
   }, {})
 
   t.is(boleto.status, 'pending_registration')
+
+  restoreFunction(provider, 'register')
+})
+
+test('registers a boleto (provider timeout -> pending)', async (t) => {
+  mockFunction(
+    provider,
+    'register',
+    always(Promise.resolve({ status: 'unknown' }).delay(6000))
+  )
+
+  const payload = mock
+
+  const { body } = await create({
+    body: payload
+  })
+
+  const boleto = await register({
+    body: JSON.stringify({
+      boleto_id: body.id,
+      sqsMessage: {
+        ReceiptHandle: 'abc'
+      }
+    })
+  }, {})
+
+  t.is(boleto.status, 'pending_registration')
+
+  restoreFunction(provider, 'register')
 })
