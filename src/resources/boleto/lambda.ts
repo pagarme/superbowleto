6530b1ab4e98f3acfcddb74d53a2696d8271c84e
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-07-27 14:18:04 -0300

    lambda: fix register lambda function name
    Fix register lambda function name to follow the format
    ${stage}-superbowleto-register-boleto.

--- src/resources/boleto/lambda.ts
+++ src/resources/boleto/lambda.ts
@@ -1,15 +1,15 @@
 import lambda from '../../lib/lambda'
 import { getEnv } from '../../config/index'
 
-function getFullLamdaFunctionName (shortName) {
+function getFullLambdaFunctionName (shortName) {
   const stage = process.env.STAGE || 'test'
 
-  return `superbowleto-${stage}-${shortName}`
+  return `${stage}-superbowleto-${shortName}`
 }
 
 function register (payload) {
   return lambda.invoke({
-    FunctionName: getFullLamdaFunctionName('boleto_register'),
+    FunctionName: getFullLambdaFunctionName('register-boleto'),
     InvocationType: 'event',
     Payload: JSON.stringify(payload)
   }).promise()
