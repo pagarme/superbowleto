9f0e0c8d9d9e66656333b79f8604cc8898424b54
claytonsilva <clayton@xdevel.com.br>
2018-04-05 19:01:43 -0300

    feat: add custom startup script with migrations job
    we add migrations script in start server script and change
    Dockerfile.server and Dockerfile.worker for the new format
    we add sequelize-cli for call migrations.

---
+++ script/start_server.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+PORT=3000
+#run entrypoint for decrypt variables
+echo "call entrypoint"
+sh -c /entrypoint.sh
+#run migrations
+echo "call migrations"
+/app/node_modules/.bin/sequelize db:migrate --config /app/src/config/database.js --migrations-path /app/database/migrations/
+#run server
+echo "call server service"
+node /app/src/bin/server.js
