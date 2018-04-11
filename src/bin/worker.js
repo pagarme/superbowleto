1161dd588ec75092d102eb7016747f71da39c597
Otávio Pace <otaviopp8@gmail.com>
2018-04-11 17:35:01 -0300

    worker: add database initialization ensurance

--- src/bin/worker.js
+++ src/bin/worker.js
@@ -3,6 +3,10 @@ const dotenv = require('dotenv')
 if (process.env.NODE_ENV === 'production' && process.env.DOTENV_PATH) {
   dotenv.config({ path: process.env.DOTENV_PATH })
 }
+
+const { DatabaseError } = require('../lib/errors')
+const database = require('../database')
+const { ensureDatabaseIsConnected } = require('../functions/database')
 const BoletoService = require('../resources/boleto/service')
 const { BoletosToRegisterQueue } = require('../resources/boleto/queues')
 const { defaultCuidValue } = require('../lib/database/schema')
@@ -18,19 +22,38 @@ const logger = makeLogger(
   { id: queueItemId }
 )
 
-BoletosToRegisterQueue.on('error', (err) => {
-  logger.error({
-    status: 'failed',
-    metadata: {
-      error_name: err.name,
-      error_stack: err.stack,
-      error_message: err.message,
-    },
+const startWorker = () => {
+  BoletosToRegisterQueue.on('error', (err) => {
+    logger.error({
+      status: 'failed',
+      metadata: {
+        error_name: err.name,
+        error_stack: err.stack,
+        error_message: err.message,
+      },
+    })
   })
-})
 
-BoletosToRegisterQueue.startProcessing(service.processBoleto, {
-  keepMessages: true,
-})
+  BoletosToRegisterQueue.startProcessing(service.processBoleto, {
+    keepMessages: true,
+  })
 
-console.log('Superbowleto worker is up')
+  console.log('Superbowleto worker is up')
+}
+
+const handleInitializationErrors = (err) => {
+  if (err instanceof DatabaseError) {
+    console.log(`Initialization failed with connection to database: ${err}`)
+    return process.exit(1)
+  }
+
+  console.log(`Unknown error: ${err}`)
+  return process.exit(1)
+}
+
+const initializeApplication = db =>
+  ensureDatabaseIsConnected(db)
+    .tap(() => startWorker())
+    .catch(handleInitializationErrors)
+
+initializeApplication(database)
