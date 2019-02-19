59bd56e81d094697e02dc621d3ac2f48ad954fd0
Jean Augusto Silva <jean.augusto@pagar.me>
2019-02-19 19:02:14 -0300

    bradesco: fix document_type when registering
    Check the length of document_number to set the correct document_type.

--- test/unit/providers/bradesco/index.js
+++ test/unit/providers/bradesco/index.js
@@ -11,7 +11,7 @@ test('buildHeaders', (t) => {
   })
 })
 
-test('buildPayload', async (t) => {
+test('buildPayload with a cpf number on company_document_number', async (t) => {
   const boleto = await createBoleto()
   const payload = buildPayload(boleto)
 
@@ -42,6 +42,55 @@ test('buildPayload', async (t) => {
         sacador_avalista: {
           nome: 'Some Company',
           documento: '98154524872',
+          tipo_documento: '1',
+          endereco: {
+            cep: '04547006',
+            logradouro: 'Rua Gomes de Carvalho',
+            numero: '1609',
+            complemento: '6º andar',
+            bairro: 'Vila Olimpia',
+            cidade: 'São Paulo',
+            uf: 'SP',
+          },
+        },
+      },
+    },
+  })
+})
+
+test('buildPayload with a cnpj number on company_document_number', async (t) => {
+  const boleto = await createBoleto({
+    company_document_number: '98154872000112',
+  })
+  const payload = buildPayload(boleto)
+
+  t.deepEqual(payload, {
+    merchant_id: '100005254',
+    boleto: {
+      carteira: '26',
+      nosso_numero: boleto.title_id,
+      numero_documento: boleto.title_id,
+      data_emissao: moment().tz('America/Sao_Paulo').format('YYYY-MM-DD'),
+      data_vencimento: moment(boleto.expiration_date).tz('America/Sao_Paulo').format('YYYY-MM-DD'),
+      valor_titulo: 2000,
+      pagador: {
+        nome: 'David Bowie',
+        documento: '98154524872',
+        tipo_documento: '1',
+        endereco: {
+          cep: '04551010',
+          logradouro: 'Rua Fidêncio Ramos',
+          numero: '308',
+          complemento: '9º andar, conjunto 91',
+          bairro: 'Vila Olímpia',
+          cidade: 'São Paulo',
+          uf: 'SP',
+        },
+      },
+      informacoes_opcionais: {
+        sacador_avalista: {
+          nome: 'Some Company',
+          documento: '98154872000112',
           tipo_documento: '2',
           endereco: {
             cep: '04547006',
