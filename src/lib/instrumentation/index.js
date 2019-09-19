ef5dd7e9d1e6dcd874e6479f6fab9b41b788afb6
Dyego Lima <dyego9444@gmail.com>
2019-09-19 18:15:28 -0300

    datadog: add hostname and port options
    Add datadog config variables to enable sending data to Datdog agent,
    that's necessary due to the fact superbowleto is running on a completely
    isolate environment

--- src/lib/instrumentation/index.js
+++ src/lib/instrumentation/index.js
@@ -1,15 +1,38 @@
 const dotenv = require('dotenv')
 const ddTrace = require('dd-trace')
+const { getCredentials } = require('../credentials')
+const { makeFromLogger } = require('../../lib/logger')
 
-const initializeNewRelic = () => {
-  if (process.env.NODE_ENV === 'production') {
-    ddTrace.init()
+const makeLogger = makeFromLogger('lib/instrumentation')
 
-    if (process.env.NEWRELIC_KEY) {
-      // eslint-disable-next-line global-require
-      require('newrelic')
-    }
+const initializeInstrumentation = () => {
+  if (process.env.NODE_ENV !== 'production') {
+    return false
   }
+
+  const logger = makeLogger({ operation: 'instrumentation' })
+
+  if (process.env.NEWRELIC_KEY) {
+    // eslint-disable-next-line global-require
+    require('newrelic')
+  }
+
+  return getCredentials('datadog/endpoint')
+    .then(datadogEndpoint =>
+      ddTrace.init({
+        hostname: datadogEndpoint,
+        port: 8126,
+      }))
+    .catch((err) => {
+      logger.error({
+        status: 'failed',
+        metadata: {
+          error_name: err.name,
+          error_stack: err.stack,
+          error_message: err.message,
+        },
+      })
+    })
 }
 
 const initializeDotEnv = () => {
@@ -20,11 +43,11 @@ const initializeDotEnv = () => {
 
 const initialize = () => {
   initializeDotEnv()
-  initializeNewRelic()
+  initializeInstrumentation()
 }
 
 module.exports = {
   initialize,
   initializeDotEnv,
-  initializeNewRelic,
+  initializeInstrumentation,
 }
