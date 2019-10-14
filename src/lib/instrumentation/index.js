775b4e34f4e68d4ae3fb334ca202105f286c5253
Dyego Lima <dyego9444@gmail.com>
2019-10-14 17:06:58 -0300

    instrumentation: remove datadog config options
    Remove datadog config options since the Datadog agent's container will
    be in the same Task Definition as the superbowleto app's container.
    
    Default values:
    
    - Endpoint: localhost
    - Port: 8126

--- src/lib/instrumentation/index.js
+++ src/lib/instrumentation/index.js
@@ -1,37 +1,14 @@
 const ddTrace = require('dd-trace')
-const { getCredentials } = require('../credentials')
-const { makeFromLogger } = require('../../lib/logger')
-
-const makeLogger = makeFromLogger('lib/instrumentation')
 
 const initInstrumentation = () => {
-  if (process.env.NODE_ENV !== 'production') {
-    return false
+  if (process.env.NODE_ENV === 'production') {
+    ddTrace.init()
+
+    if (process.env.NEWRELIC_KEY) {
+      // eslint-disable-next-line global-require
+      require('newrelic')
+    }
   }
-
-  const logger = makeLogger({ operation: 'instrumentation' })
-
-  if (process.env.NEWRELIC_KEY) {
-    // eslint-disable-next-line global-require
-    require('newrelic')
-  }
-
-  return getCredentials('datadog/endpoint')
-    .then(datadogEndpoint =>
-      ddTrace.init({
-        hostname: datadogEndpoint,
-        port: 8126,
-      }))
-    .catch((err) => {
-      logger.error({
-        status: 'failed',
-        metadata: {
-          error_name: err.name,
-          error_stack: err.stack,
-          error_message: err.message,
-        },
-      })
-    })
 }
 
 module.exports = initInstrumentation
