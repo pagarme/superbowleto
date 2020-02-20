f6c9b4adb1c7ba5115452e42f8b31682eac22c3e
lucianopf <lucianopf@outlook.com>
2020-02-20 16:58:36 -0300

    infra: new infra and ci-cd

--- src/lib/instrumentation/index.js
+++ src/lib/instrumentation/index.js
@@ -1,8 +1,11 @@
 const ddTrace = require('dd-trace')
 
 const initInstrumentation = () => {
-  if (process.env.NODE_ENV === 'production') {
-    ddTrace.init()
+  if (process.env.NODE_ENV === 'production' && process.env.STAGE !== 'stg') {
+    ddTrace.init({
+      logInjection: true,
+      runtimeMetrics: true,
+    })
 
     if (process.env.NEWRELIC_KEY) {
       // eslint-disable-next-line global-require
