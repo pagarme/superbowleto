c3e5c42f3542a30fef0b84b60f0da4d778cb3b13
alinemarjorie <aline.marjorie.af@gmail.com>
2020-09-23 13:22:56 -0300

    setBoletoRulesConfiguration: includes issuer development case

--- test/unit/lib/helper/configurations.js
+++ test/unit/lib/helper/configurations.js
@@ -3,6 +3,29 @@ import { assert } from '../../../helpers/chai'
 import { mock } from '../../../helpers/boleto'
 import { setBoletoRulesConfiguration } from '../../../../src/lib/helpers/configurations'
 
+test('setBoletoRulesConfiguration: issuer development', async (t) => {
+  const payload = mock
+
+  payload.rules = null
+  payload.issuer = 'development'
+
+  const boleto = await setBoletoRulesConfiguration(payload)
+
+  t.is(boleto.issuer_wallet, '26')
+  t.is(boleto.issuer, 'bradesco')
+
+  assert.containSubset(boleto, {
+    issuer: boleto.issuer,
+    issuer_account: boleto.issuer_account,
+    issuer_agency: boleto.issuer_agency,
+    issuer_wallet: boleto.issuer_wallet,
+    amount: payload.amount,
+    instructions: payload.instructions,
+    payer_name: payload.payer_name,
+    company_name: payload.company_name,
+  })
+})
+
 test('setBoletoRulesConfiguration: without rules', async (t) => {
   const payload = mock
 
