e3134c19bd070b31171eab9cdde167d4455e2108
Jean Augusto Silva <jean.augusto@pagar.me>
2020-12-16 10:55:38 -0300

    project: add migrate step when starting

--- script/start.sh
+++ script/start.sh
@@ -5,16 +5,20 @@ PORT=3000
 echo "call entrypoint"
 sh -c /entrypoint.sh
 
+if [ $APP_TYPE = "migrate" ]
+  #run migrations
+  echo "call migrations"
+  /app/node_modules/.bin/sequelize db:migrate --config /app/src/config/database.js --migrations-path /app/src/database/migrations/
+fi
 
 if [ $APP_TYPE = "worker" ]
 then
   # run worker
   echo "call worker service"
   node /app/src/bin/worker.js
-else
-  #run migrations
-  echo "call migrations"
-  /app/node_modules/.bin/sequelize db:migrate --config /app/src/config/database.js --migrations-path /app/src/database/migrations/
+fi
+
+if [ $APP_TYPE = "server" ]
   #run server
   echo "call server service"
   node /app/src/bin/server.js
